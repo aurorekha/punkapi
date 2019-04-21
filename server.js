1289900194503f6a76ce9c456d7930438d9a9666
Rekha Bhagavatula <rekha.bhagavatula@sofaworks.co.uk>
2019-04-21 23:17:39 +0100

    Added the grid component and the sort function

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static('public'));
+app.use(express.static('public'));
 
 app.use(express.static('src'));
 
-app.use('/accordion', express.static('accordion.html'));
+app.use('/', express.static('public/index.html'));
 
-app.use('/carousel', express.static('carousel.html'));
+app.use('/accordion', express.static('public/accordion.html'));
 
-app.use('/grid', express.static('grid.html'));
+app.use('/carousel', express.static('public/carousel.html'));
+
+app.use('/grid', express.static('public/grid.html'));
 
 app.listen(3000, function () {
   console.log('Express server is up on port 3000');
---
+++ src/js/grid.js
@@ -0,0 +1,64 @@
+window.onload = function() {
+  const div = document.getElementById('grid');
+  const url = 'https://api.punkapi.com/v2/beers';
+
+  // for creating necessary nodes to the element
+  function createElements(element) {
+    return document.createElement(element);
+  }
+
+  // for appending the children of the first element
+  function appendingChildren(parent, el) {
+    return parent.appendChild(el);
+  }
+  const getData = fetch(url)
+    .then((resp) => resp.json());
+// div class panel info
+    getData.then(function(data) {
+        data.forEach(function(beer) {
+          // creating the elements here
+          let divTwo = createElements('div'),
+           panelDiv = createElements('div'),
+           img = createElements('img'),
+           h3 = createElements('h3');
+           p = createElements('p');
+           img.src = `http://placeimg.com/300/260/beer`;
+           h3.innerHTML = `Name:${beer.name} </br>
+           Tagline: ${beer.tagline}`;
+           p.innerHTML = `Alcohol content: ${beer.abv}%`;
+           // appending elements
+           (appendingChildren(div, divTwo)).className += 'beerGrid',
+           appendingChildren(divTwo, h3),
+           appendingChildren(divTwo, img),
+           appendingChildren(divTwo, p);
+        });
+
+        $('.buttons').click(function(event) {
+      /* Act on the event */
+        const myThis = $(this);
+        const item = $('#grid');
+        const itemSort = $('div', item);
+
+        itemSort.sort(function(a, b){
+          // Get only the percentages from the whole text
+          const checkA = parseFloat($(a).text().split(' ').pop());
+          const checkB = parseFloat($(b).text().split(' ').pop());
+
+          // Check which button has been clicked and sort accordingly
+          if(myThis.hasClass('sortHigh')){
+            return (checkA > checkB) ? 1 : (checkA < checkB) ? -1 : 0; // ascending sort
+            } else {
+            return (checkA < checkB) ? 1 :  (checkA > checkB) ? -1 : 0; // descending sort
+            }
+          });
+            //loop through items and append the modified value
+            $.each(itemSort, function(index, val){
+                $(item).append(val);
+            });
+          event.preventDefault();
+        });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
